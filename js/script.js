7b3c06fe5d80b17b0e3eaa7ca2d5157f4bfd4182
LeonardoMarques7 <leonardoemcs155@gmail.com>
2023-12-02 19:07:49 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Seleção de Elementos
+
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicatorInput = document.querySelector("#multiplicator");
+const multiplicationTable = document.querySelector(
+    "#multiplication-operations"
+);
+
+const multiplicationTitle = document.querySelector(
+    "#multiplication-title span"
+);
+
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        const template = `
+            <div class="row">
+                <div class="operation">
+                    ${number} x ${i} =
+                </div>
+                <div class="result">
+                    ${result}
+                </div>
+            </div>
+        `;
+
+        const parser = new DOMParser();
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerHTML = number;
+};
+
+// Eventos
+
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicatorInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+});
